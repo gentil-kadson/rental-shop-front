decf582752e4dad12180e09d31017e7c71847202
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-02-04 10:28:16 -0300

    feature: add to cart button completed.

--- src/components/AddToCartButton/index.tsx
+++ src/components/AddToCartButton/index.tsx
@@ -1,9 +1,12 @@
 import styles from "./index.module.css";
+import { ShoppingCart } from "phosphor-react";
 
 export default function AddToCartButton() {
   return (
     <button className={styles.addToCartButton}>
-      <div></div>
+      <div>
+        <ShoppingCart color="white" size={20} />
+      </div>
       <span>Adicionar ao carrinho</span>
     </button>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import ItemCard from "@/components/ItemCard";
+import AddToCartButton from "@/components/AddToCartButton";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ItemCard />
+      <AddToCartButton />
     </>
   );
 }
