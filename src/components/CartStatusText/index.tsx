9ae33d4ed89baaedef4878e66299cd37dedbf284
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-02-04 10:48:52 -0300

    feature: finished cart status text.

---
+++ src/components/CartStatusText/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./index.module.css";
+
+export default function CartStatusText() {
+  return <span className={styles.cartStatusSpan}>Total: R$ 200,00</span>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import CartStatusText from "@/components/CartStatusText";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <CartStatusText />
     </>
   );
 }
