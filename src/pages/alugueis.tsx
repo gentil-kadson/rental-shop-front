7a75cc301b23368f185585adebf78ad6fec6c9b5
karla@solarz.com.br <karla@solarz.com.br>
2023-02-05 17:17:40 -0300

    create page 'itens alugados'

---
+++ src/pages/alugueis.tsx
@@ -0,0 +1,15 @@
+import ButtonPrimary from "@/components/ButtonPrimary";
+import Header from "@/components/Header";
+import Title from "@/components/Title";
+
+export default function Alugueis() {
+    return (
+        <>
+            <Header />
+
+            <Title title="Itens alugados" />
+
+            <ButtonPrimary title="Fechar aluguel" />
+        </>
+    )
+}
